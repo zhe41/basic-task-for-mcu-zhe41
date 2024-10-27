c1338c4c2293c4fcbc68e3ccb540e8284558dd50
zhe41 <1660956822@qq.com>
2024-10-27 21:45:40 +0800

    Update task.c

--- task.c
+++ task.c
@@ -16,15 +16,17 @@ void delay_ms(unsigned int x)  // 延时函数
 // tips: 原理图当中led为低电平点亮，比如点亮LED2,代码为： P0 = 0xFE (1111 1110)
 
 
-int main(void)
+void main()
 {
-    """
-    在下方编写你的代码
-    """
-    
+    unsigned char led = 0x01;  // 初始点亮最左边的灯
 
- 
+    while (1)
+    {
+        P0 = led;  // 输出当前点亮的灯
+        delay_ms(500);  // 延时 500ms
 
-    return 0;
+        led <<= 1;  // 左移一位，准备点亮下一个灯
+        if (led == 0)  // 如果已经移到最右边，重新从最左边开始
+            led = 0x01;
+    }
 }
-
